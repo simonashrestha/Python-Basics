379f17aaccb687c2b388bd630dd47cff7ad16316
simonashrestha <simonashrestha1@gmail.com>
2024-11-21 18:22:45 +0545

    first commit

---
+++ for_i.py
@@ -0,0 +1,4 @@
+# If you write two numbers in range then those numbers will get printed till the last number, but if you write three numbers then, the first two numbers are the range and will be added till the maximum number.
+
+for i in range(1,10,8):
+    print(i)
---
+++ if_else.py
@@ -0,0 +1,5 @@
+a= int(input("Enter any number: "))
+if (a<5):
+    print("a is smaller than 5.")
+else:
+    print("a is greater than 5.")
